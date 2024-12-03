3307f60368c3f2e3a3a7cacc6e3db44d7f79a91b
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-12-04 03:35:28 +0530

    feat: merger doc tabs
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

--- src/app/(basiclayout)/mergers-acquisitions/[id]/page.tsx
+++ src/app/(basiclayout)/mergers-acquisitions/[id]/page.tsx
@@ -5,6 +5,43 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
+import { Tabs, TabsList } from "@/components/ui/tabs";
+import { TabsContent, TabsTrigger } from "@radix-ui/react-tabs";
+
+const documentsToRender = [
+  {
+    title: "Document 1",
+    url: "https://arxiv.org/pdf/2005.11405",
+  },
+  {
+    title: "Document 2",
+    url: "https://arxiv.org/pdf/2005.11402",
+  },
+  {
+    title: "Document 3",
+    url: "https://arxiv.org/pdf/2005.11403",
+  },
+  {
+    title: "Document 4",
+    url: "https://arxiv.org/pdf/2005.11404",
+  },
+  {
+    title: "Document 5",
+    url: "https://arxiv.org/pdf/2005.11405",
+  },
+  {
+    title: "Document 6",
+    url: "https://arxiv.org/pdf/2005.11406",
+  },
+  {
+    title: "Document 7",
+    url: "https://arxiv.org/pdf/2005.11407",
+  },
+  {
+    title: "Document 8",
+    url: "https://arxiv.org/pdf/2005.11408",
+  },
+];
 
 const Page = ({ params }: { params: { id: string } }) => {
   return (
@@ -17,18 +54,37 @@ const Page = ({ params }: { params: { id: string } }) => {
         </ResizablePanel>
 
         <ResizableHandle />
-        <ResizablePanel minSize={30} defaultSize={35}>
-          <object
-            data="https://arxiv.org/pdf/2005.11401"
-            type="application/pdf"
-            width="100%"
-            height="100%"
+        <ResizablePanel minSize={30} defaultSize={35} className="h-full">
+          <Tabs
+            defaultValue="Document 1"
+            className="w-full flex flex-col p-4 h-full gap-4 max-h-screen"
           >
-            <p>
-              Alternative text - include a link{" "}
-              <a href="https://arxiv.org/pdf/2005.11401">to the PDF!</a>
-            </p>
-          </object>
+            <TabsList className="flex flex-wrap gap-2 rounded-none w-full bg-transparent h-fit">
+              {documentsToRender.map((document) => (
+                <TabsTrigger
+                  key={document.title}
+                  value={document.title}
+                  className="border px-2 rounded-lg border-[#484851] hover:bg-[#18181B] transition-all duration-150 w-40"
+                >
+                  {document.title}
+                </TabsTrigger>
+              ))}
+            </TabsList>
+            {documentsToRender.map((document) => (
+              <TabsContent
+                key={document.title}
+                value={document.title}
+                className="h-full"
+              >
+                <object
+                  data={document.url}
+                  type="application/pdf"
+                  width="100%"
+                  height="100%"
+                ></object>
+              </TabsContent>
+            ))}
+          </Tabs>
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
